e3f050babc8add2128545c121f6a7848ac06afef
AnahatChahal <82654399+AnahatChahal@users.noreply.github.com>
2021-06-02 18:31:13 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,40 @@
+img="";
+status="";
+
+function preload()
+{
+    img= loadImage('bed.jpg');
+}
+
+function setup()
+{
+    canvas= createCanvas(640, 450);
+    canvas.center();
+
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("model is loaded");
+    status=true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+}
+function draw()
+{
+    image(img,0,0,640,450);
+    fill('#6495ED');
+    noFill();
+    stroke("#6495ED");
+    rect(40,70,500,400);
+    text("Bed",45,75);
+}
